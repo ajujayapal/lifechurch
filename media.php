273b04a73d89211d4693c187c1206b781284245e
ajujayapal <jeremiahpaul@gmail.com>
2017-01-13 17:36:35 -0600

    New code for sermon audio.

--- media.php
+++ media.php
@@ -4,10 +4,79 @@
 	
 	<?php include "includes/navbar.inc.php"; ?>
 
+	<div class="container widget-box-area">
+		<div class="row">
+		
+			<div class="col-12">
+				<div class="card mb-3">
+					<div class="card-block">
 
+						<h5 class="card-title">Spiritual Lessons from the Life and Ministry of John the Baptist.</h5>
 
-	<div class="jumbotron center-text">
-		<h1>media.html - Coming Soon</h1>
+						<p class="card-text">Pastor Dennis Whitter speakingLuke 3:1-6As he relates to Jesus, John was known as:ForerunnerPrecursorAnnouncerCrierA VoiceProphetLesson One: God’s Timing Is Perfect   (Luke 3:1-2a)Lesson Two: God Can Use Anyone    (Luke 3:2b) Lesson Three: Repentance Precedes Preparation    (Luke 3:3-6)</p>
+
+						<p class="card-text sermon-date">SUN, DECEMBER 4, 2016</p>
+
+					</div>
+					
+					<div class="card-footer">
+						<button class="btn btn-primary play-button">Play Audio</button>
+					</div>
+
+					<div class="card-footer">
+						<audio controls preload="none" style="width:100%;">
+							<source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_12-4-16.m4a" type="audio/mp4" />
+							<p>Your browser does not support HTML5 audio.</p>
+						</audio>
+					</div>
+					
+				</div>
+			</div>
+		
+			<div class="col-12">
+
+				<div class="card mb-3">
+					<div class="card-block">
+
+						<h5 class="card-title">What will you be getting for Christmas's?</h5>
+
+						<p class="card-text">Examining our expectations.Pastor Dennis Whitter speakingLuke 1:25-38I. An Angel Visits MaryII. Mary Visits ElizabethIII.</p>
+
+						<p class="card-text sermon-date">SUN, NOVEMBER 27, 2016</p>
+
+					</div>
+					
+					<div class="card-footer">
+						<button class="btn btn-primary play-button">Play Audio</button>
+					</div>
+
+					<div class="card-footer">
+						<audio controls preload="none" style="width:100%;">
+							<source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_11-27-16.m4a" type="audio/mp4" />
+							<p>Your browser does not support HTML5 audio.</p>
+						</audio>
+
+					</div>
+				</div>
+			</div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+			
+			
+			
+			
+		</div>
 	</div>
+
 
 	<?php include "includes/footer.inc.php"; ?>
---
+++ scripts/sermonAudio.js
@@ -0,0 +1,10 @@
+var playButtons = document.getElementsByClassName("play-button");
+
+
+
+function toggleAudioControls() {
+	
+	var target = e.click;
+	alert(target);
+	
+}
