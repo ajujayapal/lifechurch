fc73f0806a9ea1872895df92ffa7adfd3b269e43
ajujayapal <jeremiahpaul@gmail.com>
2017-01-15 00:41:28 -0600

    adding sermon audio page and javascript

--- media.php
+++ media.php
@@ -1,82 +1,69 @@
 <!DOCTYPE html>
-	
-	<?php include "includes/header.inc.php"; ?>
-	
-	<?php include "includes/navbar.inc.php"; ?>
 
-	<div class="container widget-box-area">
-		<div class="row">
-		
-			<div class="col-12">
-				<div class="card mb-3">
-					<div class="card-block">
+<?php include "includes/header.inc.php"; ?>
 
-						<h5 class="card-title">Spiritual Lessons from the Life and Ministry of John the Baptist.</h5>
+<?php include "includes/navbar.inc.php"; ?>
 
-						<p class="card-text">Pastor Dennis Whitter speakingLuke 3:1-6As he relates to Jesus, John was known as:ForerunnerPrecursorAnnouncerCrierA VoiceProphetLesson One: God’s Timing Is Perfect   (Luke 3:1-2a)Lesson Two: God Can Use Anyone    (Luke 3:2b) Lesson Three: Repentance Precedes Preparation    (Luke 3:3-6)</p>
+<div class="container widget-box-area">
+    <div class="row">
 
-						<p class="card-text sermon-date">SUN, DECEMBER 4, 2016</p>
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="card-block">
 
-					</div>
-					
-					<div class="card-footer">
-						<button class="btn btn-primary play-button">Play Audio</button>
-					</div>
+                    <h5 class="card-title">Spiritual Lessons from the Life and Ministry of John the Baptist.</h5>
 
-					<div class="card-footer">
-						<audio controls preload="none" style="width:100%;">
-							<source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_12-4-16.m4a" type="audio/mp4" />
-							<p>Your browser does not support HTML5 audio.</p>
-						</audio>
-					</div>
-					
-				</div>
-			</div>
-		
-			<div class="col-12">
+                    <p class="card-text">Pastor Dennis Whitter speakingLuke 3:1-6As he relates to Jesus, John was known as:ForerunnerPrecursorAnnouncerCrierA VoiceProphetLesson One: God’s Timing Is Perfect (Luke 3:1-2a)Lesson Two: God Can Use Anyone (Luke 3:2b) Lesson Three: Repentance Precedes Preparation (Luke 3:3-6)</p>
 
-				<div class="card mb-3">
-					<div class="card-block">
+                    <p class="card-text sermon-date">SUN, DECEMBER 4, 2016</p>
 
-						<h5 class="card-title">What will you be getting for Christmas's?</h5>
+                </div>
 
-						<p class="card-text">Examining our expectations.Pastor Dennis Whitter speakingLuke 1:25-38I. An Angel Visits MaryII. Mary Visits ElizabethIII.</p>
+                <div class="card-footer">
+                    <button class="btn btn-primary play-button">Play Audio</button>
+                </div>
 
-						<p class="card-text sermon-date">SUN, NOVEMBER 27, 2016</p>
+                <div class="card-footer collapse">
+                    <audio controls preload="none" style="width:100%;">
+                        <source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_12-4-16.m4a" type="audio/mp4" />
+                        <p>Your browser does not support HTML5 audio.</p>
+                    </audio>
+                </div>
 
-					</div>
-					
-					<div class="card-footer">
-						<button class="btn btn-primary play-button">Play Audio</button>
-					</div>
+            </div>
+        </div>
 
-					<div class="card-footer">
-						<audio controls preload="none" style="width:100%;">
-							<source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_11-27-16.m4a" type="audio/mp4" />
-							<p>Your browser does not support HTML5 audio.</p>
-						</audio>
+        <div class="col-12">
 
-					</div>
-				</div>
-			</div>
-		
-		
-		
-		
-		
-		
-		
-		
-		
-			
-			
-			
-			
-			
-			
-			
-		</div>
-	</div>
+            <div class="card mb-3">
+                <div class="card-block">
+
+                    <h5 class="card-title">What will you be getting for Christmas's?</h5>
+
+                    <p class="card-text">Examining our expectations.Pastor Dennis Whitter speakingLuke 1:25-38I. An Angel Visits MaryII. Mary Visits ElizabethIII.</p>
+
+                    <p class="card-text sermon-date">SUN, NOVEMBER 27, 2016</p>
+
+                </div>
+
+                <div class="card-footer">
+                    <button class="btn btn-primary play-button">Play Audio</button>
+                </div>
+
+                <div class="card-footer collapse">
+                    <audio controls preload="none" style="width:100%;">
+                        <source src="http://www.lifechurchinfo.org/Websites/lifechurchinfo/Blog/4348935/Sermon_11-27-16.m4a" type="audio/mp4" />
+                        <p>Your browser does not support HTML5 audio.</p>
+                    </audio>
+
+                </div>
+            </div>
+        </div>
 
 
-	<?php include "includes/footer.inc.php"; ?>
+
+    </div>
+</div>
+
+
+<?php include "includes/footer.inc.php"; ?>
--- scripts/sermonAudio.js
+++ scripts/sermonAudio.js
@@ -1,10 +1,19 @@
 var playButtons = document.getElementsByClassName("play-button");
 
-
+for(var i = 0; i < playButtons.length; i++) {
+    playButtons[i].addEventListener("click", toggleAudioControls, false);
+}
 
 function toggleAudioControls() {
 	
-	var target = e.click;
-	alert(target);
-	
+    var cardFooter = this.parentElement.nextElementSibling;
+	cardFooter.classList.toggle("collapse");
+    
+    if (cardFooter.classList.contains("collapse")) {
+       this.innerHTML = "Play Audio";        
+    }
+    
+    else {
+        this.innerHTML = "Stop Audio";
+    }
 }
