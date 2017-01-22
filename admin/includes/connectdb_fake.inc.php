2ffb1a20f77f0e3b7677c57e9541cf0c13ef4536
ajujayapal <jeremiahpaul@gmail.com>
2017-01-22 12:02:28 -0600

    changed hostname

--- admin/includes/connectdb_fake.inc.php
+++ admin/includes/connectdb_fake.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new PDO("mysql:host=108.167.140.78:3306;dbname=fake_db;charset=utf8mb4", 'fake_username', 'fake_password');
+$db = new PDO("mysql:host=localhost;dbname=fake_db;charset=utf8mb4", 'fake_username', 'fake_password');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 /*$db->setAttribute(PDO::ATT_EMULATE_PREPARES, false);*/
 
